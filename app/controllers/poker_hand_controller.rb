52bff82c95155bdee18b6f1f808cb972c64c11ca
johnathanthedev <johnathanguzman17@gmail.com>
2021-10-20 19:02:16 -0500

    Barebones. Need to remove 'is ranking type' from model'

--- app/controllers/poker_hand_controller.rb
+++ app/controllers/poker_hand_controller.rb
@@ -10,10 +10,5 @@ class PokerHandController < ApplicationController
     user_poker_hand = params[:poker_hand].split(' ')
     poker_hand_service = PokerHandService.new(user_poker_hand)
     @ranked_hand = poker_hand_service.rank_hand
-    render 'submit_hand'
-    # respond_to do |format|
-    #   format.html { render :submit_hand }
-    # end
-    # render partial: 'submit_hand'
   end
 end
---
+++ app/views/poker_hands/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @poker_hands, partial: "poker_hands/poker_hand", as: :poker_hand
